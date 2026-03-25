febdfb7e19ddbb5b80d4eba69663286246ec2066
ghizlane-bouaddi <gizlanebouaddi378@gmail.com>
2026-03-25 22:04:21 +0100

    day2 ex3

--- Semaine1/day1/day2/ex3.js
+++ Semaine1/day1/day2/ex3.js
@@ -1,8 +1,65 @@
-let ventes  =[
-    {mois : 'march',chiffre_affaires : 30000, nb_clients :10, ville :'youssoufia'},
-    {mois : 'march',chiffre_affaires : 50000, nb_clients :10, ville :'youssoufia'},
-    {mois : 'mai',chiffre_affaires : 52000, nb_clients :15, ville :'casa'},
-    {mois : 'avrile',chiffre_affaires :31000, nb_clients :20, ville :'rbate'},
-    {mois : 'mai',chiffre_affaires : 14000, nb_clients :25, ville :'youssoufia'}
+let recettes =[
+    {pseudo :"shein", note : 2,commentaire : "nice", date : 2025},
+    {pseudo :"jumia", note : 3,commentaire : "nice", date : 2026},
+    {pseudo :"jumia", note : 1,commentaire : "nice", date : 2026},
+    {pseudo :"shein", note : 4,commentaire : "niceee", date : 2022},
+    {pseudo :"temu", note : 4,commentaire : "nice", date : 2021}
 ]
+
+let total = 0;
+
+for(let i=0;i<recettes.length;i++){
+    total += recettes[i].note;
+}
+
+let M =0;
+
+M = total / recettes.length;
+console.log(total);
+console.log(M);
+console.log(Math.round(M));
+
+let N ={};
+
+for(let i=0;i<recettes.length;i++){
+    let note = recettes[i].note;
+
+    if(!N[note]){
+        N[note] =0;
+    }
+
+    N[note] ++;
+}
+
+console.log(N);
+
+
+for(let i=0;i<recettes.length;i++){
+if(recettes[i].note >3){
+    console.log(`positifs`);
+}else{
+    console.log(`négatifs `);
+}
+}
+
+let note = recettes.sort(function(a,b){
+    return a.note - b.note;
+});
+
+for(let i=0;i<recettes.length;i++){
+    console.log(recettes[i].note);
+}
+
+let max = recettes[0];
+for(let i=0;i<recettes.length;i++){
+    // console.log(recettes[i].pseudo);
+    // console.log(recettes[i].commentaire.length);
+    // console.log(max.commentaire.length);
+    
+    if(recettes[i].commentaire.length > max.commentaire.length){
+       max = recettes[i];
+    }
+}
+
+console.log(max);
 
